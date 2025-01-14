3920e1453a124b3e4b8f19df116a4a5d5ea4ba5f
Wieland <29458682+wielandb@users.noreply.github.com>
2025-01-14 15:24:14 +0100

    Export after DPO merge

--- export/web/finetune-collector.service.worker.js
+++ export/web/finetune-collector.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1736620926|1241544802';
+const CACHE_VERSION = '1736864632|26056302';
 /** @type {string} */
 const CACHE_PREFIX = 'finetune-collect-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

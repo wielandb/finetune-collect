946950c5878e9d9f99b0b29d509b8dce124dc2c3
Wieland <29458682+wielandb@users.noreply.github.com>
2025-06-24 00:54:24 +0200

    Export for release

--- export/web/index.service.worker.js
+++ export/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747301448|4369229587';
+const CACHE_VERSION = '1750719224|28662186765';
 /** @type {string} */
 const CACHE_PREFIX = 'finetune-collect-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

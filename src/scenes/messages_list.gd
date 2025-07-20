9354150e895eccabeb340cfe5e58621360dade6f
Wieland <29458682+wielandb@users.noreply.github.com>
2025-07-20 21:34:34 +0200

    Update OpenAI addon

--- src/scenes/messages_list.gd
+++ src/scenes/messages_list.gd
@@ -119,7 +119,7 @@ func delete_all_messages_from_UI():
 func models_received(models: Array[String]):
 	print(models)
 
-func gpt_response_completed(message:Message, response:Dictionary):
+func gpt_response_completed(message: Message, response:Dictionary):
 	printt(message.get_as_dict())
 	# Add a new message to the MessagesListContainer
 	var MessageInstance = MESSAGE_SCENE.instantiate()
@@ -265,6 +265,8 @@ func _on_add_message_completion_button_pressed() -> void:
 		current_msg_ix += 1
 	var model = settings["modelChoice"]
 	print(model)
+	for m in openai_messages:
+		print(m.content)
 	var toolsforopenAI = get_tree().get_root().get_node("FineTune/Conversation/Functions/FunctionsList").functions_list_to_gpt_available_tools_list()
 	openai.prompt_gpt(openai_messages, model, "https://api.openai.com/v1/chat/completions", toolsforopenAI)
 

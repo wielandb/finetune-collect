b587e2d6356631910d5260b0797397262c5ddd0e
Wieland <29458682+wielandb@users.noreply.github.com>
2025-07-22 11:21:20 +0200

    Add grader list and container logic

---
+++ src/grader_container.gd
@@ -0,0 +1,20 @@
+extends VBoxContainer
+
+@onready var GRADER_SCENES = [
+	preload("res://scenes/graders/string_check_grader.tscn"),
+	preload("res://scenes/graders/string_similarity_grader.tscn"),
+	preload("res://scenes/graders/score_model_grader.tscn"),
+	preload("res://label_model_grader.tscn"),
+	preload("res://scenes/graders/python_grader.tscn")
+]
+
+func _ready() -> void:
+	$GraderHeaderMarginContainer/LabelAndChoiceBoxContainer/GraderTypeOptionButton.connect("item_selected", self, "_on_grader_type_option_button_item_selected")
+	_on_grader_type_option_button_item_selected($GraderHeaderMarginContainer/LabelAndChoiceBoxContainer/GraderTypeOptionButton.selected)
+
+func _on_grader_type_option_button_item_selected(index: int) -> void:
+	for child in $ActualGraderContainer.get_children():
+	    child.queue_free()
+	if index >= 0 and index < GRADER_SCENES.size():
+	    var inst = GRADER_SCENES[index].instantiate()
+	    $ActualGraderContainer.add_child(inst)

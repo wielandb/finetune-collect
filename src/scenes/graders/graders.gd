1fe7a1f9b112d05549a0abbd440f950836424c1d
Wieland <29458682+wielandb@users.noreply.github.com>
2025-07-22 11:21:52 +0200

    Add grader list and container logic (#110)

--- src/scenes/graders/graders.gd
+++ src/scenes/graders/graders.gd
@@ -1,1 +1,9 @@
 extends ScrollContainer
+
+@onready var GRADER_SCENE = preload("res://grader_container.tscn")
+
+func _on_add_grader_button_pressed() -> void:
+	var inst = GRADER_SCENE.instantiate()
+	$GradersListContainer.add_child(inst)
+	$GradersListContainer.move_child($GradersListContainer/AddGraderButton, -1)
+

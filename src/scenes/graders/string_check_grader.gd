18c6c5f029b67e20c38d34cab0a559857bfc574b
Wieland <29458682+wielandb@users.noreply.github.com>
2025-07-22 13:36:07 +0200

    Add string check grader from/to var

---
+++ src/scenes/graders/string_check_grader.gd
@@ -0,0 +1,16 @@
+extends VBoxContainer
+
+func to_var():
+	var me = {}
+	me["type"] = "string_check"
+	me["name"] = $NameContainer.grader_name
+	me["input"] = $GridContainer/InputEdit.text
+	me["reference"] = $GridContainer/ReferenceEdit.text
+	me["operation"] = $GridContainer/OperationOptionButton.get_item_text($GridContainer/OperationOptionButton.selected)
+		
+func from_var(grader_data):
+	$NameContainer.grader_name = grader_data.get("name", "")
+	$GridContainer/InputEdit.text = grader_data.get("input", "")
+	$GridContainer/ReferenceEdit.text = grader_data.get("reference")
+	$GridContainer/OperationOptionButton.select(0)
+	$GridContainer/OperationOptionButton.select($GridContainer/OperationOptionButton.get_item_index(grader_data.get("operation", "eq")))

5ddf5f95f3e3c75f05c908c7bf230dfa2818a16b
Wieland <29458682+wielandb@users.noreply.github.com>
2025-07-30 20:42:44 +0200

    Ensure MultiGrader instantiates default graders (#115)

--- src/scenes/graders/multi_grader.gd
+++ src/scenes/graders/multi_grader.gd
@@ -3,4 +3,7 @@ extends VBoxContainer
 @onready var GRADER_SCENE = preload("res://scenes/graders/grader_container.tscn")
 
 func _ready() -> void:
-	pass
+	if $MarginContainer.get_child_count() == 0:
+		$MarginContainer.add_child(GRADER_SCENE.instantiate())
+	if $MarginContainer2.get_child_count() == 0:
+		$MarginContainer2.add_child(GRADER_SCENE.instantiate())

1f90317028b13caa49b5ac3e135574f14287495d
Wieland <29458682+wielandb@users.noreply.github.com>
2025-07-31 21:30:54 +0200

    Make deleting labels from label model grader work

--- src/scenes/graders/label_model_grader.gd
+++ src/scenes/graders/label_model_grader.gd
@@ -1,6 +1,6 @@
 extends VBoxContainer
 
-	var passing_icon = preload("res://icons/check-decagram-custom.png")
+var passing_icon = preload("res://icons/check-decagram-custom.png")
 var MESSAGE_SCENE = preload("res://scenes/message.tscn")
 
 func _ready() -> void:
@@ -40,8 +40,7 @@ func _on_labels_list_item_activated(index: int) -> void:
 		$LabelsList.set_item_icon(index, passing_icon)
 	$LabelsList.deselect_all()
 
-func _on_labels_list_item_right_clicked(index: int) -> void:
-	$LabelsList.remove_item(index)
+
 
 func _on_new_label_button_pressed() -> void:
 	var text = $NewLabelsContainer/NewLabelLabel2.text
@@ -60,3 +59,7 @@ func _on_add_message_button_pressed() -> void:
 	msg._on_message_type_item_selected(msg.get_node("MessageSettingsContainer/MessageType").selected)
 	$MessagesContainer.add_child(container)
 	$MessagesContainer.move_child($MessagesContainer/AddMessageButton, -1)
+
+func _on_labels_list_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
+	if mouse_button_index == MOUSE_BUTTON_RIGHT:
+		$LabelsList.remove_item(index)

3aa6b750a49e41ac6d1e7de2163ca3b311814eb6
Wieland <29458682+wielandb@users.noreply.github.com>
2025-07-31 21:17:38 +0200

    Fix label model grader UI interactions

--- src/scenes/graders/label_model_grader.gd
+++ src/scenes/graders/label_model_grader.gd
@@ -1,6 +1,13 @@
 extends VBoxContainer
 
-var passing_icon = preload("res://icons/check-decagram-custom.png")
+	var passing_icon = preload("res://icons/check-decagram-custom.png")
+var MESSAGE_SCENE = preload("res://scenes/message.tscn")
+
+func _ready() -> void:
+	for child in $MessagesContainer.get_children():
+		var msg = child.get_node_or_null("Message")
+		if msg:
+			msg._on_message_type_item_selected(msg.get_node("MessageSettingsContainer/MessageType").selected)
 
 func to_var():
 	var me = {}
@@ -32,3 +39,24 @@ func _on_labels_list_item_activated(index: int) -> void:
 	else:
 		$LabelsList.set_item_icon(index, passing_icon)
 	$LabelsList.deselect_all()
+
+func _on_labels_list_item_right_clicked(index: int) -> void:
+	$LabelsList.remove_item(index)
+
+func _on_new_label_button_pressed() -> void:
+	var text = $NewLabelsContainer/NewLabelLabel2.text
+	if text != "":
+		$LabelsList.add_item(text)
+		$NewLabelsContainer/NewLabelLabel2.text = ""
+		$LabelsList.deselect_all()
+
+func _on_add_message_button_pressed() -> void:
+	var container = MarginContainer.new()
+	container.layout_mode = 2
+	container.add_theme_constant_override("margin_left", 90)
+	container.add_theme_constant_override("margin_right", 95)
+	var msg = MESSAGE_SCENE.instantiate()
+	container.add_child(msg)
+	msg._on_message_type_item_selected(msg.get_node("MessageSettingsContainer/MessageType").selected)
+	$MessagesContainer.add_child(container)
+	$MessagesContainer.move_child($MessagesContainer/AddMessageButton, -1)

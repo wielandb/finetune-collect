f9d2a68599960502e93e43e9f1b3320413e6b247
Wieland <29458682+wielandb@users.noreply.github.com>
2025-08-02 01:23:03 +0200

    fix: ensure MultiGrader uses constant for grader scene

--- src/scenes/graders/multi_grader.gd
+++ src/scenes/graders/multi_grader.gd
@@ -1,6 +1,6 @@
 extends VBoxContainer
 
-@onready var GRADER_SCENE = preload("res://scenes/graders/grader_container.tscn")
+const GRADER_SCENE := preload("res://scenes/graders/grader_container.tscn")
 
 func _ready() -> void:
 	pass

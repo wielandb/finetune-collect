f5b6edee08e1ad97b0569d1bb86501e057c4942c
Wieland <29458682+wielandb@users.noreply.github.com>
2025-08-02 02:57:43 +0200

    feat: debounce grader verification

--- src/scenes/graders/grader_container.gd
+++ src/scenes/graders/grader_container.gd
@@ -9,8 +9,15 @@ extends VBoxContainer
 	preload("res://scenes/graders/multi_grader.tscn")
 ]
 
+var _verify_timer: Timer
+
 func _ready() -> void:
 	$GraderHeaderMarginContainer/LabelAndChoiceBoxContainer/GraderTypeOptionButton.connect("item_selected", _on_grader_type_option_button_item_selected)
+	_verify_timer = Timer.new()
+	_verify_timer.one_shot = true
+	_verify_timer.wait_time = 2.0
+	add_child(_verify_timer)
+	_verify_timer.connect("timeout", Callable(self, "_on_verify_timeout"))
 	_on_grader_type_option_button_item_selected($GraderHeaderMarginContainer/LabelAndChoiceBoxContainer/GraderTypeOptionButton.selected)
 
 func _on_grader_type_option_button_item_selected(index: int) -> void:
@@ -19,9 +26,32 @@ func _on_grader_type_option_button_item_selected(index: int) -> void:
 	if index >= 0 and index < GRADER_SCENES.size():
 		var inst = GRADER_SCENES[index].instantiate()
 		$ActualGraderContainer/GraderMarginContainer.add_child(inst)
+		_connect_gui_input_signals(inst)
 
 func _on_delete_button_pressed() -> void:
 	queue_free()
-	
+
 func verify_grader() -> bool:
 	return true
+
+func _on_verify_timeout() -> void:
+	verify_grader()
+
+func _schedule_verify() -> void:
+	_verify_timer.start()
+
+func _on_any_gui_input(event: InputEvent) -> void:
+	_schedule_verify()
+
+func _on_child_entered(child: Node) -> void:
+	_connect_gui_input_signals(child)
+
+func _connect_gui_input_signals(node: Node) -> void:
+	if node is Control:
+		if not node.is_connected("gui_input", Callable(self, "_on_any_gui_input")):
+			node.connect("gui_input", Callable(self, "_on_any_gui_input"))
+	if not node.is_connected("child_entered_tree", Callable(self, "_on_child_entered")):
+		node.connect("child_entered_tree", Callable(self, "_on_child_entered"))
+	for child in node.get_children():
+		_connect_gui_input_signals(child)
+

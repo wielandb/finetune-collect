a8e921d02a41d81425cb16491041869344c9755a
Wieland <29458682+wielandb@users.noreply.github.com>
2025-08-03 18:19:39 +0200

    Ensure GridContainer stays after AddGrader button (#148)

--- src/scenes/graders/graders.gd
+++ src/scenes/graders/graders.gd
@@ -5,25 +5,32 @@ extends ScrollContainer
 func _on_add_grader_button_pressed() -> void:
 	var inst = GRADER_SCENE.instantiate()
 	$GradersListContainer.add_child(inst)
-	$GradersListContainer.move_child($GradersListContainer/AddGraderButton, -1)
+	$GradersListContainer.move_child($GradersListContainer/SampleItemsContainer, -1)
+	var btn_index = $GradersListContainer.get_children().find($GradersListContainer/AddGraderButton)
+	$GradersListContainer.move_child(inst, btn_index)
+
 
 func to_var():
 	var all = []
 	for child in $GradersListContainer.get_children():
-		if child.name == "AddGraderButton":
+		if child.name == "AddGraderButton" or child.name == "SampleItemsContainer":
 			continue
 		if child.has_method("to_var"):
 			all.append(child.to_var())
 	return all
 
+
 func from_var(graders_data):
 	for child in $GradersListContainer.get_children():
-		if child.name != "AddGraderButton":
+		if child.name != "AddGraderButton" and child.name != "SampleItemsContainer":
 			child.queue_free()
+	$GradersListContainer.move_child($GradersListContainer/SampleItemsContainer, -1)
 	if graders_data is Array:
 		for g in graders_data:
 			var inst = GRADER_SCENE.instantiate()
 			$GradersListContainer.add_child(inst)
-			$GradersListContainer.move_child($GradersListContainer/AddGraderButton, -1)
+			$GradersListContainer.move_child($GradersListContainer/SampleItemsContainer, -1)
+			var btn_index = $GradersListContainer.get_children().find($GradersListContainer/AddGraderButton)
+			$GradersListContainer.move_child(inst, btn_index)
 			if inst.has_method("from_var"):
 				inst.from_var(g)

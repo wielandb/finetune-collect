f52e0d503be4656bc0f7495b8ab444bb3aa707bb
Wieland <29458682+wielandb@users.noreply.github.com>
2025-08-04 12:37:17 +0200

    getters/setters for copyabledatabutton

--- src/scenes/graders/copy_able_data_container.gd
+++ src/scenes/graders/copy_able_data_container.gd
@@ -1,5 +1,16 @@
 extends HBoxContainer
 
-@export var dataStr = ""
+@export var dataStr: String:
+	get:
+		return $CopyAbleDataContentLabel.text
+	set(value):
+		$CopyAbleDataContentLabel.text = str(value)
 
-@export var copyable = true
+@export var copyable: bool:
+	get:
+		return not $CopyButton.disabled
+	set(value):
+		if value:
+			$CopyButton.disabled = false
+		else:
+			$CopyButton.disabled = true

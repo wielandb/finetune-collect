89d472b635ddbe6482ef9f90423caa10ab3ad76b
Wieland <29458682+wielandb@users.noreply.github.com>
2025-08-14 17:03:20 +0200

    fix: infer sanitized variable type (#175)

--- src/scenes/schemas/json_schema_container.gd
+++ src/scenes/schemas/json_schema_container.gd
@@ -1,9 +1,19 @@
 extends HBoxContainer
 
+const SchemaAlignOpenAI := preload("res://scenes/schemas/schema_align_openai.gd")
+
 
 func _on_delete_schema_button_pressed() -> void:
 	queue_free()
 
 
 func _on_edit_json_schema_code_edit_text_changed() -> void:
-	pass # TODO: Automatically set the OAI JSON Schema Edit text to this text but sanitized
+	var editor := $MarginContainer2/SchemasTabContainer/EditSchemaTabBar/VBoxContainer/EditJSONSchemaCodeEdit
+	var oai_editor := $MarginContainer2/SchemasTabContainer/OAISchemaTabBar/VBoxContainer/OAIJSONSchemaCodeEdit
+	var json := JSON.new()
+	var err := json.parse(editor.text)
+	if err != OK:
+		oai_editor.text = ""
+		return
+	var sanitized = SchemaAlignOpenAI.sanitize_envelope_or_schema(json.data)
+	oai_editor.text = JSON.stringify(sanitized, "\t")

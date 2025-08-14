af9d36637649473ed6edbd316502539533ab627d
Wieland <29458682+wielandb@users.noreply.github.com>
2025-08-14 17:56:07 +0200

    Sync schema title with name field (#178)

--- src/scenes/schemas/json_schema_container.gd
+++ src/scenes/schemas/json_schema_container.gd
@@ -2,6 +2,8 @@ extends HBoxContainer
 
 const SchemaAlignOpenAI := preload("res://scenes/schemas/schema_align_openai.gd")
 
+var _updating_from_name := false
+
 
 func _on_delete_schema_button_pressed() -> void:
 	queue_free()
@@ -10,6 +12,7 @@ func _on_delete_schema_button_pressed() -> void:
 func _on_edit_json_schema_code_edit_text_changed() -> void:
 	var editor := $MarginContainer2/SchemasTabContainer/EditSchemaTabBar/VBoxContainer/EditJSONSchemaCodeEdit
 	var oai_editor := $MarginContainer2/SchemasTabContainer/OAISchemaTabBar/VBoxContainer/OAIJSONSchemaCodeEdit
+	var name_edit := $MarginContainer/JSONSchemaControlsContainer/SchemaNameContainer/LineEdit
 	var json := JSON.new()
 	var err := json.parse(editor.text)
 	if err != OK:
@@ -17,3 +20,19 @@ func _on_edit_json_schema_code_edit_text_changed() -> void:
 		return
 	var sanitized = SchemaAlignOpenAI.sanitize_envelope_or_schema(json.data)
 	oai_editor.text = JSON.stringify(sanitized, "\t")
+	if not _updating_from_name and json.data is Dictionary and json.data.has("title"):
+		var title = json.data["title"]
+		if title is String:
+			name_edit.text = title
+
+
+func _on_schema_name_line_edit_text_changed(new_text: String) -> void:
+	var editor := $MarginContainer2/SchemasTabContainer/EditSchemaTabBar/VBoxContainer/EditJSONSchemaCodeEdit
+	var json := JSON.new()
+	var err := json.parse(editor.text)
+	if err != OK or not (json.data is Dictionary):
+		return
+	_updating_from_name = true
+	json.data["title"] = new_text
+	editor.text = JSON.stringify(json.data, "\t")
+	_updating_from_name = false

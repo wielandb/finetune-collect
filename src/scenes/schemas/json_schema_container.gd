8867401ea249eff10f5523a3c0725dc06c7efbdb
Wieland <29458682+wielandb@users.noreply.github.com>
2025-08-14 16:47:17 +0200

    Add connection

--- src/scenes/schemas/json_schema_container.gd
+++ src/scenes/schemas/json_schema_container.gd
@@ -3,3 +3,7 @@ extends HBoxContainer
 
 func _on_delete_schema_button_pressed() -> void:
 	queue_free()
+
+
+func _on_edit_json_schema_code_edit_text_changed() -> void:
+	pass # TODO: Automatically set the OAI JSON Schema Edit text to this text but sanitized
